193ce7ce754ee456a603d69212c4bb6a8e70904b
despkontopoulou <despkontopoulou@gmail.com>
2025-01-04 17:24:34 +0200

    side menu implementation

--- TMLocationSelection.cs
+++ TMLocationSelection.cs
@@ -16,12 +16,83 @@ namespace SmartTent
         {
             InitializeComponent();
             this.DoubleBuffered = true;
+            SubPanelTent.Visible = true;
+            SubPanelExplore.Visible = false;
+            SubPanelEmergency.Visible = false;
+            MainExploreNearby.Location = new Point(0, 214);
+            MainEmergencyNav.Location = new Point(0, 245);
         }
 
-        private void panel1_Paint(object sender, PaintEventArgs e)
+
+        private void button3_Click(object sender, EventArgs e)
         {
 
         }
 
+        private void button1_Click(object sender, EventArgs e)
+        {
+            SubPanelTent.Visible = !SubPanelTent.Visible;//toggle buttons
+            if (SubPanelTent.Visible)
+            {
+                SubPanelExplore.Visible = false;
+                SubPanelEmergency.Visible = false;
+            }
+            placeButtons();
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            SubPanelExplore.Visible = !SubPanelExplore.Visible;
+            if (SubPanelExplore.Visible)
+            {
+                SubPanelTent.Visible = false;
+                SubPanelEmergency.Visible = false;
+            }
+            placeButtons();
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+            SubPanelEmergency.Visible = !SubPanelEmergency.Visible;
+            if (SubPanelEmergency.Visible)
+            {
+                SubPanelTent.Visible = false;
+                SubPanelExplore.Visible = false;
+            }
+            placeButtons();
+        }
+
+        private void placeButtons()
+        {
+            if (SubPanelTent.Visible)
+            {
+                MainExploreNearby.Location = new Point(0, 214);
+                MainEmergencyNav.Location = new Point(0, 245);
+
+            }
+            if (!SubPanelTent.Visible)
+            {
+                MainExploreNearby.Location = new Point(0, 32);
+                MainEmergencyNav.Location = new Point(0, 65);
+
+            }
+
+            if (SubPanelExplore.Visible)
+            {
+                MainExploreNearby.Location = new Point(0, 32);
+                SubPanelExplore.Location = new Point(34, 245);
+                MainEmergencyNav.Location = new Point(0, 203);
+            }
+            if (SubPanelEmergency.Visible)
+            {
+                SubPanelEmergency.Location = new Point(0, 98);
+            }
+
+        }
+
+        private void TMLocationSelection_Load(object sender, EventArgs e)
+        {
+
+        }
     }
 }
